3e80ba05d602dbb3e6071f0a083a37f77dce30c7
Laptop <Laptop@LAPTOP-94IP7ASU>
2025-03-08 09:27:51 +0700

    Chinh sua trang thong tin

--- QuanLyQuanAn1/frmaccount.Designer.cs
+++ QuanLyQuanAn1/frmaccount.Designer.cs
@@ -38,34 +38,49 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.lblpassword = new System.Windows.Forms.Label();
             this.btnthoat = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.lbltendangnhap);
-            this.panel1.Location = new System.Drawing.Point(1, 14);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 70);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(547, 49);
+            this.panel1.Size = new System.Drawing.Size(490, 74);
             this.panel1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 10);
+            this.textBox1.Location = new System.Drawing.Point(161, 26);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(237, 25);
+            this.textBox1.Size = new System.Drawing.Size(237, 21);
             this.textBox1.TabIndex = 1;
             // 
             // lbltendangnhap
             // 
             this.lbltendangnhap.AutoSize = true;
-            this.lbltendangnhap.Location = new System.Drawing.Point(29, 13);
+            this.lbltendangnhap.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltendangnhap.Location = new System.Drawing.Point(29, 27);
             this.lbltendangnhap.Name = "lbltendangnhap";
-            this.lbltendangnhap.Size = new System.Drawing.Size(98, 17);
+            this.lbltendangnhap.Size = new System.Drawing.Size(110, 20);
             this.lbltendangnhap.TabIndex = 0;
             this.lbltendangnhap.Text = "Tên đăng nhập:";
             // 
@@ -73,25 +88,27 @@
             // 
             this.panel2.Controls.Add(this.textBox2);
             this.panel2.Controls.Add(this.lbltenhienthi);
-            this.panel2.Location = new System.Drawing.Point(1, 69);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 150);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(547, 49);
+            this.panel2.Size = new System.Drawing.Size(490, 78);
             this.panel2.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(141, 10);
+            this.textBox2.Location = new System.Drawing.Point(161, 33);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(237, 25);
+            this.textBox2.Size = new System.Drawing.Size(237, 21);
             this.textBox2.TabIndex = 1;
             // 
             // lbltenhienthi
             // 
             this.lbltenhienthi.AutoSize = true;
-            this.lbltenhienthi.Location = new System.Drawing.Point(29, 13);
+            this.lbltenhienthi.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltenhienthi.Location = new System.Drawing.Point(29, 34);
             this.lbltenhienthi.Name = "lbltenhienthi";
-            this.lbltenhienthi.Size = new System.Drawing.Size(77, 17);
+            this.lbltenhienthi.Size = new System.Drawing.Size(88, 20);
             this.lbltenhienthi.TabIndex = 0;
             this.lbltenhienthi.Text = "Tên hiển thị:";
             // 
@@ -99,47 +116,141 @@
             // 
             this.panel3.Controls.Add(this.textBox3);
             this.panel3.Controls.Add(this.lblpassword);
-            this.panel3.Location = new System.Drawing.Point(1, 124);
+            this.panel3.Location = new System.Drawing.Point(3, 234);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(547, 49);
+            this.panel3.Size = new System.Drawing.Size(490, 72);
             this.panel3.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(141, 10);
+            this.textBox3.Location = new System.Drawing.Point(161, 27);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(237, 25);
+            this.textBox3.Size = new System.Drawing.Size(237, 21);
             this.textBox3.TabIndex = 1;
             // 
             // lblpassword
             // 
             this.lblpassword.AutoSize = true;
-            this.lblpassword.Location = new System.Drawing.Point(29, 13);
+            this.lblpassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpassword.Location = new System.Drawing.Point(38, 28);
             this.lblpassword.Name = "lblpassword";
-            this.lblpassword.Size = new System.Drawing.Size(73, 17);
+            this.lblpassword.Size = new System.Drawing.Size(79, 20);
             this.lblpassword.TabIndex = 0;
             this.lblpassword.Text = "Pass Word:";
             // 
             // btnthoat
             // 
-            this.btnthoat.Location = new System.Drawing.Point(295, 179);
+            this.btnthoat.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnthoat.ForeColor = System.Drawing.Color.Black;
+            this.btnthoat.Location = new System.Drawing.Point(389, 39);
             this.btnthoat.Name = "btnthoat";
             this.btnthoat.Size = new System.Drawing.Size(84, 39);
             this.btnthoat.TabIndex = 3;
             this.btnthoat.Text = "Thoát";
-            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.UseVisualStyleBackColor = false;
             this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.65854F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.21951F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel5, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel6, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(820, 436);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.tableLayoutPanel2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(156, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(496, 430);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(658, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(159, 430);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(3, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(147, 430);
+            this.panel6.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.panel7, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.panel8, 0, 4);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.5814F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.60465F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.53488F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.13953F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.67442F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(496, 430);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thong tin chi tiet";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label1);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel7.Location = new System.Drawing.Point(3, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(490, 61);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.btnthoat);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel8.Location = new System.Drawing.Point(3, 312);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(490, 115);
+            this.panel8.TabIndex = 3;
             // 
             // frmaccount
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 260);
-            this.Controls.Add(this.btnthoat);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(820, 436);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -153,6 +264,12 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -169,5 +286,13 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label lblpassword;
         private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel8;
     }
 }
--- QuanLyQuanAn1/frmaccount.cs
+++ QuanLyQuanAn1/frmaccount.cs
@@ -26,5 +26,10 @@ namespace QuanLyQuanAn1
         {
 
         }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
